694518082411a8249d79e58f5320035447ac5599
Manvita Joshi <manvitaajoshi@gmail.com>
2021-03-19 21:43:37 +0530

    User info displayed while editing

--- app/src/main/java/com/example/credeb/UserProfileUpdate.java
+++ app/src/main/java/com/example/credeb/UserProfileUpdate.java
@@ -1,5 +1,6 @@
 package com.example.credeb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,14 +11,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class UserProfileUpdate extends AppCompatActivity {
+import com.example.credeb.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserProfileUpdateActivity extends AppCompatActivity {
 
     EditText fname;
     EditText lname;
     EditText mobile;
     EditText email;
     Button update_button;
-
+    private FirebaseUser firebaseUser;
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +38,9 @@ public class UserProfileUpdate extends AppCompatActivity {
         mobile = findViewById(R.id.mobile);
         email = findViewById(R.id.email);
         update_button = findViewById(R.id.update_button);
-
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        userID = firebaseUser.getUid();
+        userinfo();
         update_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,10 +77,30 @@ public class UserProfileUpdate extends AppCompatActivity {
         update_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(UserProfileUpdate.this, "Update successful", Toast.LENGTH_SHORT).show();
+                Toast.makeText(UserProfileUpdateActivity.this, "Update successful", Toast.LENGTH_SHORT).show();
 
             }
         });
 
     }
-}
+    private void userinfo() {
+        FirebaseDatabase.getInstance().getReference().child("App Database").child("User details").child(userID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                assert user != null;
+                // usernameTextView.setText(user.getFname());
+                //String txt = user.getFname() + " " + user.getLname();
+                fname.setText(user.getFname());
+                lname.setText(user.getLname());
+                mobile.setText(user.getMobile());
+                email.setText(user.getEmail_ID());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+}}
