9e6f0eb068571c0cf7be652966be45dcd795c0b3
Manvita Joshi <manvitaajoshi@gmail.com>
2021-04-03 13:22:42 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/credeb/Adapter/ViewTransactionHistoryAdapter.java
@@ -0,0 +1,110 @@
+package com.example.credeb.Adapter;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+import com.example.credeb.Model.TransactionHistory;
+import com.example.credeb.Model.UserBankDetails;
+import com.example.credeb.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ViewTransactionHistoryAdapter<firebaseUser> extends ArrayAdapter {
+
+    private Activity mContext;
+    List<TransactionHistory> transactionHistoryActivity;
+    private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    String account;
+
+
+    public ViewTransactionHistoryAdapter(Activity mContext, List<TransactionHistory> transactionHistoryActivity) {
+        super(mContext, R.layout.trans_list_view, transactionHistoryActivity);
+        this.mContext = mContext;
+        this.transactionHistoryActivity = transactionHistoryActivity;
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = mContext.getLayoutInflater();
+        View listItemView = inflater.inflate(R.layout.trans_list_view, null, true);
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String userID = user.getUid();
+        TextView bank_name = listItemView.findViewById(R.id.textView13);
+        TextView account_no = listItemView.findViewById(R.id.textView14);
+        TextView ifsc_code = listItemView.findViewById(R.id.textView24);
+        TextView balance = listItemView.findViewById(R.id.textView25);
+        TextView balance1 = listItemView.findViewById(R.id.textView112);
+        DatabaseReference reference2 = FirebaseDatabase.getInstance().getReference().child("App Database").child("User bank details");
+        final String[] accountNumber = new String[1];
+        reference2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("sender", "hi " + accountNumber[0]);
+                for (DataSnapshot snap : snapshot.getChildren()) {
+                    Log.d("sender", "hi in " + accountNumber[0]);
+                    UserBankDetails userBankDetails = snap.getValue(UserBankDetails.class);
+                    assert userBankDetails != null;
+                    String val = userBankDetails.getUserID();
+                    String ifsc = userBankDetails.getIfsc();
+                    Log.d("acc", "get val " + val);
+                    if (val.equals(userID)) {
+                        accountNumber[0] = userBankDetails.getAccount_no();
+
+                        TransactionHistory transactionHistory = transactionHistoryActivity.get(position);
+                        if (accountNumber[0].equals(transactionHistory.getSender()) && (transactionHistory.getType_of_transaction()).equals("Debit")) {
+                            Log.d("ADAPTER", "acc sender" + accountNumber[0]);
+                            bank_name.setText("Receiver: " + transactionHistory.getReceiver());
+                            balance.setText("Receiver IFSC: " + transactionHistory.getReceiver_IFSC());
+                            account_no.setText("Amount: " + transactionHistory.getAmount());
+                            ifsc_code.setText("Date and time: " + transactionHistory.getDate_and_Time());
+                            balance1.setText("Type of transaction: " + transactionHistory.getType_of_transaction());
+                        }
+                        if (accountNumber[0].equals(transactionHistory.getReceiver()) && (transactionHistory.getType_of_transaction()).equals("Credit")) {
+                            Log.d("ADAPTER", "acc receiver" + accountNumber[0]);
+                            bank_name.setText("Sender: " + transactionHistory.getSender());
+                            balance.setText("Sender IFSC: " + transactionHistory.getSender_IFSC());
+                            account_no.setText("Amount: " + transactionHistory.getAmount());
+                            ifsc_code.setText("Date and time: " + transactionHistory.getDate_and_Time());
+                            balance1.setText("Type of transaction: " + transactionHistory.getType_of_transaction());
+
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        return listItemView;
+
+    }
+}
+
