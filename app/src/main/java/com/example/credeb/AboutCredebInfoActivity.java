1af2f88d68cb4f854179280f52de0a62e289f51f
Manvita Joshi <manvitaajoshi@gmail.com>
2021-04-03 13:21:30 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/credeb/AboutCredebInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.credeb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutCredebInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_credeb_info);
+    }
+}
